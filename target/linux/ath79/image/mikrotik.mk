6591c2216ce1ad902510d62c99812314b20b1ed5
Zafar Iqbal <zafar.amt@gmail.com>
2022-03-24 12:00:50 +0530

    this is copied from GGBRUNO and now modifying for komikan board, first checkin

---
+++ target/linux/ath79/image/mikrotik.mk
@@ -0,0 +1,29 @@
+include ./common-mikrotik.mk
+
+define Device/mikrotik_routerboard-493g
+  $(Device/mikrotik_nand)
+  SOC := ar7161
+  DEVICE_MODEL := RouterBOARD 493G
+  DEVICE_PACKAGES += kmod-usb-ohci kmod-usb2
+  SUPPORTED_DEVICES += rb-493g
+endef
+TARGET_DEVICES += mikrotik_routerboard-493g
+
+define Device/mikrotik_routerboard-922uags-5hpacd
+  $(Device/mikrotik_nand)
+  SOC := qca9558
+  DEVICE_MODEL := RouterBOARD 922UAGS-5HPacD
+  DEVICE_PACKAGES += kmod-ath10k-ct ath10k-firmware-qca988x-ct kmod-usb2
+  SUPPORTED_DEVICES += rb-922uags-5hpacd
+endef
+TARGET_DEVICES += mikrotik_routerboard-922uags-5hpacd
+
+define Device/mikrotik_routerboard-wap-g-5hact2hnd
+  $(Device/mikrotik_nor)
+  SOC := qca9556
+  DEVICE_MODEL := RouterBOARD wAP G-5HacT2HnD (wAP AC)
+  IMAGE_SIZE := 16256k
+  DEVICE_PACKAGES += kmod-ath10k-ct-smallbuffers ath10k-firmware-qca988x-ct
+  SUPPORTED_DEVICES += rb-wapg-5hact2hnd
+endef
+TARGET_DEVICES += mikrotik_routerboard-wap-g-5hact2hnd
